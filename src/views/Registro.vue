d7b3cd92a010c522c6c19feb9edca2793f7b9874
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-11-28 08:22:02 +0100

    formulario registro con estilos añadidos

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,15 +1,41 @@
 <template>
-	<h1>Registro de Usuario</h1>
-	<form>
-		<input type="email" placeholder="email" />
-		<input type="password" placeholder="password" />
-		<input type="password" placeholder="password" />
-		<button type="submit">Registrar</button>
-	</form>
+	<section class="registro">
+		<h1>Registro de Usuario</h1>
+		<form>
+			<input type="email" placeholder="email" class="form-control m-2" />
+			<input type="password" placeholder="password" class="form-control m-2" />
+			<input type="password" placeholder="password" class="form-control m-2" />
+			<button type="submit" class="btn btn-primary">Registrar</button>
+		</form>
+	</section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.registro {
+	text-align: center;
+	width: 50%;
+	max-width: 50rem;
+	margin: 0 auto;
+	background-color: #323232;
+	padding: 2rem;
+	margin-top: 10rem;
+	border-radius: 15px;
+	h1 {
+		color: white;
+		margin-bottom: 3rem;
+	}
+	input {
+		max-width: 15rem;
+		margin: 1rem auto 0 auto;
+		display: inline-block;
+	}
+	button {
+		display: block;
+		margin: 3rem auto 0 auto;
+	}
+}
+</style>
